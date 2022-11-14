f9da04da0c728c034304edbf715c3372a1b0bec8
Muzammil Abdul Rehman <muzammil.abdul.rehman@gmail.com>
2022-11-14 09:50:25 -0800

    [TST] Adding Basic Tests for Reads

--- ip-firewall-reloader/ipfirewall/ipf_test.go
+++ ip-firewall-reloader/ipfirewall/ipf_test.go
@@ -1,9 +1,54 @@
 package ipfirewall
 
+import (
+	"testing"
+	"time"
+)
+
 /****************/
 /*     Tests    */
 /****************/
 
+func TestNewIPFirewallIsActive(t *testing.T) {
+	i := new(IPFirewall)
+	if i.IsActive() {
+		t.Fatalf("TestNewIPFirewallIsActive: The default state of the firewall must be `%s`. Found `%s`.", disabledStr, i.mode)
+	}
+}
+
+func TestVersionIncrements(t *testing.T) {
+	ip := new(IPFirewall)
+	var versionIncrements uint64 = 1000
+	// incremement versions
+	var i uint64
+	for i = 0; i < versionIncrements; i++ {
+		ip.IncVersion()
+	}
+	// test increments
+	if ip.versionNumber != versionIncrements {
+		t.Fatalf("TestVersionIncrements: Unexpected number of version increments. Expected `%d`. Found `%d`.", versionIncrements, ip.versionNumber)
+	}
+}
+
 /****************/
 /*  Benchmarks  */
 /****************/
+func BenchmarkReadVersion(b *testing.B) {
+
+	ip := new(IPFirewall)
+	ip.versionNumber = uint64(time.Now().Unix())
+
+	for i := 0; i < b.N; i++ {
+		ip.ReadVersion()
+	}
+}
+
+func BenchmarkReadEventuallyConsistentVersion(b *testing.B) {
+
+	ip := new(IPFirewall)
+	ip.versionNumber = uint64(time.Now().Unix())
+
+	for i := 0; i < b.N; i++ {
+		ip.ReadEventuallyConsistentVersion()
+	}
+}
